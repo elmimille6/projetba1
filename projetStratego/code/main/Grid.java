9f091b71ce80549dd8974a529fd4391c6742fd97
elmimille6@MSI <mimille6@gmail.com>
2015-02-23 15:15:54 +0100

    add save TODO fix load

--- projetStratego/code/main/Grid.java
+++ projetStratego/code/main/Grid.java
@@ -1,139 +1,177 @@
 package main;
 
-import pawn.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import pawn.APawn;
+import pawn.Lake;
 
 /**
-* Grid is the class that creates a "grid" object.
-*
-* @author CAREDDA Giuliano, DUCOBU Alexandre
-*/
-public class Grid {
-	private APawn[][] grid; 
-	private int row,line;
-	
+ * Grid is the class that creates a "grid" object.
+ * 
+ * @author CAREDDA Giuliano, DUCOBU Alexandre
+ */
+public class Grid implements java.io.Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8927958880942845647L;
+	private APawn[][] grid;
+	private int row, line;
+
 	/**
 	 * Constructor of the grid.
 	 * 
 	 * @param size
-	 *    Size of the grid.
+	 *            Size of the grid.
 	 */
-	public Grid(int size){
-		row=size;
-		line=size;
+	public Grid(int size) {
+		row = size;
+		line = size;
 		grid = new APawn[line][row];
 		APawn lake = new Lake();
-		grid[4][2]= lake;
-		grid[4][3]= lake;
-		grid[5][2]= lake;
-		grid[5][3]= lake;
-		grid[4][6]= lake;
-		grid[4][7]= lake;
-		grid[5][6]= lake;
-		grid[5][7]= lake;
+		grid[4][2] = lake;
+		grid[4][3] = lake;
+		grid[5][2] = lake;
+		grid[5][3] = lake;
+		grid[4][6] = lake;
+		grid[4][7] = lake;
+		grid[5][6] = lake;
+		grid[5][7] = lake;
 	}
-	
+
 	/**
 	 * Put the pawn on the grid.
 	 * 
 	 * @param i
-	 *    Line of the grid.
-	 *
+	 *            Line of the grid.
+	 * 
 	 * @param j
-	 *    Column of the grid.
-	 *
+	 *            Column of the grid.
+	 * 
 	 * @param x
-	 *    The pawn to put.
+	 *            The pawn to put.
 	 */
-	public void set(int i, int j, APawn x){
-		grid[i][j]=x;
-		if (x instanceof APawn){
-		x.setPos(i,j);
+	public void set(int i, int j, APawn x) {
+		grid[i][j] = x;
+		if (x instanceof APawn) {
+			x.setPos(i, j);
 		}
 	}
-	
+
 	/**
 	 * Displays the grid.
 	 */
-	public void showGrid(){
-		for (int i = 0;i<grid.length;i++){
-			for (int j = 0; j<grid[0].length;j++){
-				if (grid[i][j]==null){
+	public void showGrid() {
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if (grid[i][j] == null) {
 					System.out.print("n     ");
-				}
-				else{
-					System.out.print(grid[i][j]+"    ");
+				} else {
+					System.out.print(grid[i][j] + "    ");
 				}
 			}
 			System.out.println();
 		}
 		System.out.println();
 	}
-	
-//	/**
-//	 * Check if the pawn can move.
-//	 * 
-//	 * @param i 
-//	 *    Line of the pawn.
-//	 *
-//	 * @param j
-//	 *    Column of the pawn.
-//	 *
-//	 * @return
-//	 *    A boolean: true or false.
-//	 */
-//	public boolean pawnCanMove(int i, int j){
-//		APawn pawn = grid[i][j];
-//		if (pawn!= null){
-//			if (pawn.canMove()){
-//				if (i!=0){
-//					if (grid[i-1][j]==null){
-//						return true;
-//					}
-//				}
-//			}
-//		}
-//		return false;
-//	}
-	
+
+	// /**
+	// * Check if the pawn can move.
+	// *
+	// * @param i
+	// * Line of the pawn.
+	// *
+	// * @param j
+	// * Column of the pawn.
+	// *
+	// * @return
+	// * A boolean: true or false.
+	// */
+	// public boolean pawnCanMove(int i, int j){
+	// APawn pawn = grid[i][j];
+	// if (pawn!= null){
+	// if (pawn.canMove()){
+	// if (i!=0){
+	// if (grid[i-1][j]==null){
+	// return true;
+	// }
+	// }
+	// }
+	// }
+	// return false;
+	// }
+
 	/**
 	 * Gets the pawn at the given coordinates.
 	 * 
-	 * @param i 
-	 *    Line of the pawn.
-	 *
-	 * @param j
-	 *    Column of the pawn.
+	 * @param i
+	 *            Line of the pawn.
 	 * 
-	 * @return
-	 *    Pawn at the given coordinates.
+	 * @param j
+	 *            Column of the pawn.
+	 * 
+	 * @return Pawn at the given coordinates.
 	 */
-	public APawn get(int i, int j){
+	public APawn get(int i, int j) {
 		return grid[i][j];
 	}
-	
-	public void placeTeam(APawn[][] tgrid,int side){
-		if (side==1){
-			for (int i =0;i<tgrid.length;i++){
-				for (int j=0;j<tgrid[0].length;j++){
-//					grid[6+i][0+j]=tgrid[i][j];
-					this.set(6+i, 0+j, tgrid[i][j]);
+
+	public void placeTeam(APawn[][] tgrid, int side) {
+		if (side == 1) {
+			for (int i = 0; i < tgrid.length; i++) {
+				for (int j = 0; j < tgrid[0].length; j++) {
+					// grid[6+i][0+j]=tgrid[i][j];
+					this.set(6 + i, 0 + j, tgrid[i][j]);
 				}
 			}
 		}
-		if (side==2){
-			for (int i =0;i<tgrid.length;i++){
-				for (int j=0;j<tgrid[0].length;j++){
-//					grid[3-i][9-j]=tgrid[i][j];
-//					tgrid[i][j].setPos(3-i, 9-j);
-					this.set(3-i, 9-j, tgrid[i][j]);
+		if (side == 2) {
+			for (int i = 0; i < tgrid.length; i++) {
+				for (int j = 0; j < tgrid[0].length; j++) {
+					// grid[3-i][9-j]=tgrid[i][j];
+					// tgrid[i][j].setPos(3-i, 9-j);
+					this.set(3 - i, 9 - j, tgrid[i][j]);
 				}
 			}
 		}
 	}
-	public int getRow(){
-		return row-1;
+
+	public int getRow() {
+		return row - 1;
 	}
-	public int getLine(){
-		return line-1;
+
+	public int getLine() {
+		return line - 1;
+	}
+
+	public void save() {
+		try {
+			ObjectOutputStream out = new ObjectOutputStream(
+					new FileOutputStream("grid.save"));
+			out.writeObject(this);
+			out.close();
+		} catch (IOException e) {
+
+		}
+	}
+
+	public static Grid load() {
+		try {
+			final FileInputStream fichier = new FileInputStream("grid.save");
+			ObjectInputStream ois = new ObjectInputStream(fichier);
+			final Grid grid =(Grid) ois.readObject();
+			ois.close();
+			
+			return grid;
+		} catch (ClassNotFoundException e1) {
+			
+		} catch (IOException e2) {
+			
+		}
+		return null;
 	}
 }
--- projetStratego/code/main/Main.java
+++ projetStratego/code/main/Main.java
@@ -12,7 +12,8 @@ public class Main {
 
 	public static void main(String[] args){ 
 //		Window fenetre = new Window();
-		Grid test = new Grid(10);
+//		Grid test = new Grid(10);
+		Grid test = Grid.load();
 		
 //		Scout p1 = new Scout(1);
 //		Miner p2 = new Miner(2);
@@ -21,7 +22,8 @@ public class Main {
 //		test.set(2, 2, p1);
 //		test.set(2, 5, p2);
 //		test.set(2, 6, p3);
-//		test.showGrid();
+		test.showGrid();
+//		test.save();
 //		System.out.println(p2.movePoss(test, 2, 2));
 //		p1.move(test, 2, 6);
 //		test.showGrid();
@@ -29,7 +31,7 @@ public class Main {
 //		test.showGrid();
 //		p2.move(test, 2, 2);
 //		test.showGrid();
-		Main.play();
+//		Main.play();
 		
 	}
 	public static void play(){
